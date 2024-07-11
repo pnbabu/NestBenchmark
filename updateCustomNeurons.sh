8d9f2cbd03c0300ca1ab72e4b8378c6b31627d8a
Pooja Babu <p.babu@fz-juelich.de>
2024-07-11 10:39:02 +0200

    Modify benchmark scripts

--- updateCustomNeurons.sh
+++ updateCustomNeurons.sh
@@ -1,25 +1,6 @@
 #!/bin/bash
 source ../nest-simulator-install/bin/nest_vars.sh
 
-
-cd Running/targets_iaf_psc_exp/target_optimized
-rm -rf CMakeCache.txt CMakeFiles
-cmake .
-make -j24 install
-cd ../..
-
-cd Running/targets_iaf_psc_exp/target
-rm -rf CMakeCache.txt CMakeFiles
-cmake .
-make -j24 install
-cd ../..
-
-cd Running/targets_iaf_psc_exp/target_plastic
-rm -rf CMakeCache.txt CMakeFiles
-cmake .
-make -j24 install
-cd ../..
-
 cd Running/targets_aeif_psc_alpha/target_optimized
 rm -rf CMakeCache.txt CMakeFiles
 cmake .
@@ -33,6 +14,13 @@ make -j24 install
 cd ../..
 
 cd Running/targets_aeif_psc_alpha/target_plastic
+rm -rf CMakeCache.txt CMakeFiles
+cmake .
+make -j24 install
+cd ../..
+
+
+cd Running/targets_aeif_psc_alpha/target_plastic_noco
 rm -rf CMakeCache.txt CMakeFiles
 cmake .
 make -j24 install
